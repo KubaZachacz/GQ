74bfba4878729af48a1e5e3815a1956c189f7884
Kuba <jakzacha@gmail.com>
2019-06-18 00:14:54 +0200

    Updates

---
+++ precache-manifest.83b7392c0f57e8bb6e4503c0ba0fef76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bd8e29cfef35189da5bbbf08fcf576",
+    "url": "/GQ/index.html"
+  },
+  {
+    "revision": "7e44feda1bcf59e86b2b",
+    "url": "/GQ/static/css/main.3cbf0cd3.chunk.css"
+  },
+  {
+    "revision": "0c7810097f865f7b394c",
+    "url": "/GQ/static/js/2.86759459.chunk.js"
+  },
+  {
+    "revision": "7e44feda1bcf59e86b2b",
+    "url": "/GQ/static/js/main.449bae9f.chunk.js"
+  },
+  {
+    "revision": "a7ad6d88e0e2ec3b1b1c",
+    "url": "/GQ/static/js/runtime~main.228948dd.js"
+  },
+  {
+    "revision": "c27ae5d564303dadc13bc4acfd2c48b1",
+    "url": "/GQ/static/media/background.c27ae5d5.jpg"
+  },
+  {
+    "revision": "0e44b4789a7ad83068e65f369c166c01",
+    "url": "/GQ/static/media/logo.0e44b478.png"
+  }
+]);
